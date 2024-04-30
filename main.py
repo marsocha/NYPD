9b0840828e73c9a96b4e5133edc47d30f4f7081b
Marcin Socha <ms418253@students.mimuw.edu.pl>
2024-04-30 19:33:28 +0200

    kopiuje z wydzialu'

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import random
+import string
+
+week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+pory = ['mor', 'eve']
+
+for day in week:
+    for when in pory:
+        path = os.path.join('./week/', day, when)
+        os.makedirs(path)
+        path_file = os.path.join(path, 'Solution.csv')
+        os.mknod(path_file)
+        with open(path_file, "w") as file:
+            file.write("Model; Output value; Time of computation; \n")
+            first = str(random.choice(('A', 'B', 'C')))
+            second = str(random.choice(range(0,1000,1)))
+            third = str(random.choice(range(1,1000,1))) + 's'
+            file.write(first + '; ' +  second + '; ' + third)
+
+def search_tmp(path):
+    for folder in os.listdir():
+        if os.path.isfile(folder):
+            f = open(folder, "r")
+            f.readline()
+            if (f.read(1)) == 'A':
+                while (f.read(1) != ';'):
+                    pass
+                while (f.read(1) != ';'):
+                    pass
+                print(f.read(2))
+                print('aaa')
+        else:
+            search(os.path.join(path, folder))
+
+def search(path):
+    for day in week:
+        for when in pory:
+            path = os.path.join('./week/', day, when)
+            os.makedirs(path)
+            path_file = os.path.join(path, 'Solution.csv')
+            os.mknod(path_file)
+            with open(path_file, "r") as file:
+                f.readline()
+                if f.read(1) == 'A':
+
+
+
+    for folder in week:
+        if os.path.isfile(folder):
+            f = open(folder, "r")
+            f.readline()
+            if (f.read(1)) == 'A':
+                while (f.read(1) != ';'):
+                    pass
+                while (f.read(1) != ';'):
+                    pass
+                print(f.read(2))
+                print('aaa')
+        else:
+            search(os.path.join(path, folder))
+
+
+search('./week')
+
+
+
