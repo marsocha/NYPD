1ea8403da5aac2fae42ecd42b246b3fce2ad8dfc
marsocha <107067776+marsocha@users.noreply.github.com>
2024-05-28 16:30:11 +0200

    Update zad10_Marcin_Socha.py

--- zad10_Marcin_Socha.py
+++ zad10_Marcin_Socha.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+##### zad1
 rows, columns = 1000, 1000
-
-
 i_tab = np.arange(rows).reshape(rows, 1)
 j_tab = np.arange(columns).reshape(1, columns)
-
 M = 2 * i_tab - j_tab
-
 print(M)
+
+##### zad2
+def test_equality(x):
+    return x**2 + 2*x-1 == (x + 2)*x-1
